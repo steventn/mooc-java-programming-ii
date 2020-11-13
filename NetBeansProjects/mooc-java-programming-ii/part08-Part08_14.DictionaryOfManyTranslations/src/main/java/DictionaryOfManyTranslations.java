1dfbd31ea46f71e1777367bcf486e4ea18fd0756
steventn <53480750+steventn@users.noreply.github.com>
2020-11-12 23:41:29 -0800

    8-14 assignment submission

---
+++ NetBeansProjects/mooc-java-programming-ii/part08-Part08_14.DictionaryOfManyTranslations/src/main/java/DictionaryOfManyTranslations.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Steven
+ */
+public class DictionaryOfManyTranslations {
+    private HashMap<String, ArrayList<String>> dictionary;
+    
+    public DictionaryOfManyTranslations() {
+        this.dictionary = new HashMap<>();
+    }
+    
+    public void add(String word, String translation) {
+        if (!dictionary.containsKey(word)) {
+            dictionary.put(word, new ArrayList<>());
+        } 
+
+        dictionary.get(word).add(translation);
+    }
+    
+    public ArrayList<String> translate(String word) {
+        if (dictionary.containsKey(word)) {
+            return dictionary.get(word);
+        }
+        return new ArrayList<String>();
+    }
+    
+    public void remove(String word) {
+        dictionary.remove(word);
+    }
+}
