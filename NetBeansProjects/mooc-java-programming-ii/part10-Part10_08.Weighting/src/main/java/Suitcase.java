6dd605a1102258b4ec3c2846213b93a0e69852c2
steventn <53480750+steventn@users.noreply.github.com>
2020-12-16 08:23:09 -0800

    10-08 assignment submission

---
+++ NetBeansProjects/mooc-java-programming-ii/part10-Part10_08.Weighting/src/main/java/Suitcase.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+
+public class Suitcase {
+
+    private int maximumWeight;
+    private ArrayList<Item> items;
+
+    public Suitcase(int maximumWeight) {
+        this.items = new ArrayList<>();
+        this.maximumWeight = maximumWeight;
+    }
+
+    public void addItem(Item Item) {
+        if (this.totalWeight() + Item.getWeight() > this.maximumWeight) {
+            return;
+        }
+
+        this.items.add(Item);
+    }
+
+    public int totalWeight() {
+          return items.stream()
+                  .mapToInt(item -> item.getWeight())
+                  .sum();
+    }
+
+    public void printItems() {
+        items.stream()
+                .forEach(suitcase -> System.out.println(suitcase));
+    }
+
+    public Item heaviestItem() {
+        if (this.items.isEmpty()) {
+            return null;
+        }
+
+        // Tutustumme järjestämiseen hieman myöhemmin kurssilla
+        return this.items.stream().max((t1, t2) -> t1.getWeight() - t2.getWeight()).get();
+    }
+
+    @Override
+    public String toString() {
+        if (this.items.isEmpty()) {
+            return "no items (0 kg)";
+        }
+
+        if (this.items.size() == 1) {
+            return "1 item (" + this.totalWeight() + " kg)";
+        }
+
+        return this.items.size() + " items (" + this.totalWeight() + " kg)";
+    }
+}
