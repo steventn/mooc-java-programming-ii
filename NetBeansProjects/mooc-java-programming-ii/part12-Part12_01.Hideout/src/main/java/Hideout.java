4170d3f8b44a92200551e647ff7557ecccead574
Steven Nguyen <steventn96@gmail.com>
2021-05-04 23:38:56 -0700

    Assignment 12-01 submission

---
+++ NetBeansProjects/mooc-java-programming-ii/part12-Part12_01.Hideout/src/main/java/Hideout.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Steven
+ * @param <T>
+ * 
+ */
+
+
+public class Hideout<T> {
+    private List<T> hideoutList;
+    
+    public Hideout() {
+        this.hideoutList = new ArrayList<>();
+    }
+    
+    public void putIntoHideout(T toHide) {
+        if (!hideoutList.isEmpty()) {
+            hideoutList.clear();
+        }
+        this.hideoutList.add(toHide);
+    }
+    
+    public T takeFromHideout() {
+        T value = hideoutList.get(0);
+        hideoutList.clear();
+        return value;
+    }
+    
+    public boolean isInHideout() {
+        if (hideoutList.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
